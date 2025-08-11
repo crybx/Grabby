356cae5cf909275944a8a75133f0c91da660ac03
crybx <crybx@users.noreply.github.com>
2025-08-11 16:33:20 -0400

    premium text update

--- content-scripts/grab-actions.js
+++ content-scripts/grab-actions.js
@@ -130,7 +130,8 @@ async function checkForPremiumContent(selectors = ["h2, h3"], duplicateCheck = t
         "Paid Content",
         "Please Login or Register First",
         "Login to buy access to the advanced chapters.",
-        "This is a premium chapter"
+        "This is a premium chapter",
+        "Secret Pavilion"
     ];
     
     for (const selector of selectors) {
